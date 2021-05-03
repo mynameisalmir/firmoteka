4d8e4c63daa746cdd89fd34e780de2113a89192b
mynameisalmir <blsw@outlook.com>
2021-05-03 15:08:37 +0200

    Dodaj nedovrsenu metodu za scrape firmi

---
+++ backend/functions/html-scraper.js
@@ -0,0 +1,86 @@
+const axios = require("axios");
+const queryString = require("qs");
+
+exports.handler = async (event) => {
+  // Parsiraj json dobijen iz requesta
+  let request = JSON.parse(event.body);
+
+  // Pripremi podatke za slanje axiosom
+  let config = setupAxios(request);
+
+  // Posalji zahtjev
+  var response = await axios(config);
+  let sliced = response.data;
+
+  // Ako ne dobijes podatke o licima prekini izvrsavanje lambde
+  if (sliced.indexOf("podaciLica") == -1 || sliced.indexOf("[[") == -1) {
+    return {
+      status: 404,
+      body: JSON.stringify({ message: "There is no data for scraping" }),
+    };
+  }
+
+  // Formiraj niz od dobijenih podataka i odradi eval kako bi od stringa napravio niz
+  sliced = sliced.slice(sliced.indexOf("[["));
+  sliced = eval(sliced.substring(0, sliced.indexOf("]];")) + "]];");
+
+  // Proslijedi dobijeni niz funkciji koja ce spremiti podatke za DynamoDB
+
+
+  return {
+    status: 200,
+    body: JSON.stringify(sliced),
+  };
+};
+
+function prepareData(array) {
+  let registrationNumber = array[1];
+  let pib = array[3];
+}
+
+function setupAxios(request) {
+  var data = queryString.stringify({
+    selectedTab: request.selectedTab,
+    urmasica: request.urmasica,
+    NAZIV_DRUSTVA_brzo: request.NAZIV_DRUSTVA_brzo,
+    REG_BR_brzo: request.REG_BR_brzo,
+    PIB_brzo: request.PIB_brzo,
+    JMBG_brzo: request.JMBG_brzo,
+    NAZIV_DRUSTVA: request.NAZIV_DRUSTVA,
+    REG_BROJ: request.REG_BROJ,
+    PIB: request.PIB,
+    SIFRA_DJELATNOSTI: request.SIFRA_DJELATNOSTI,
+    ID_PRIVREDNE_DJELATNOSTI: request.ID_PRIVREDNE_DJELATNOSTI,
+    OPSTINA_SJEDISTE: request.OPSTINA_SJEDISTE,
+    STATUS_DRUSTVA: request.STATUS_DRUSTVA,
+    DATUM_REGISTRACIJE_PROMJENE: request.DATUM_REGISTRACIJE_PROMJENE,
+    LICE: request.LICE,
+    JMBG: request.JMBG,
+    PRIKAZATI: request.PRIKAZATI,
+    SORTIRANJE: request.SORTIRANJE,
+    BROJ_PREDMETA: request.BROJ_PREDMETA,
+    DATUM_PRIJEMA: request.DATUM_PRIJEMA,
+  });
+
+  var config = {
+    method: "post",
+    url: "http://www.pretraga.crps.me:8083/Home/TraziSubmit",
+    headers: {
+      Connection: "keep-alive",
+      "Cache-Control": "max-age=0",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+      Origin: "http://www.pretraga.crps.me:8083",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      Referer: "http://www.pretraga.crps.me:8083/Home/TraziSubmit",
+      "Accept-Language": "en-US,en;q=0.9",
+      Cookie: "ASP.NET_SessionId=" + request.SessionId,
+    },
+    data: data,
+  };
+
+  return config;
+}
